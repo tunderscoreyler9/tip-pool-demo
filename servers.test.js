b0fa1423827c8f6ec9d43492017ee4a87af8ab8d
Tyler Skidmore <tyler.s9@proton.me>
2023-02-07 13:53:35 -0500

    servers.js tests made

--- servers.test.js
+++ servers.test.js
@@ -11,7 +11,32 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not accept a name-less server input into submitServerInfo()', function() {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+
+  });
+
+  it('should update the #servertable on updateServerTable() with an input', function () {
+    // call the two functions to input the server-name value
+    submitServerInfo();
+    updateServerTable();
+
+    // select the updated/inputted data set
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(curTdList.length).toEqual(2);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+  });
+
   afterEach(function() {
-    // teardown logic
+    // teardown logic: remove 'Alice' from the DOM after each test
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
+
